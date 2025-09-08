2206b622c74620864117ef1b5c2c0724f14fb50f
Jeffrey-20 <160232321@student.rnngroup.ac.uk>
2025-09-08 15:56:41 +0100

    Django CRUD

--- website/urls.py
+++ website/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name =""),
     path('login', views.login, name="login"),
-
-    
+    path('register', views.register, name="register"),
+    path('logout',views.logout, name="logout")
+  
 ]
--- website/views.py
+++ website/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
-
-
+from django.shortcuts import render,redirect
+from .forms import CreateUserForm, LoginForm
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
 
 
 
@@ -9,8 +10,46 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'pages/index.html')
 
-def login(request):
-    return render(request, 'pages/login.html')
+def logout(request):
+    auth.logout(request)
+    return redirect('')
 
+def login(request):
+    form = LoginForm()
+
+    if request.method =="POST":
+        form = LoginForm(request, data=request.POST)
+
+        if form.is_valid():
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request,username=username,password = password)
+
+            if user is not None:
+                auth.login(request,user)
+                return redirect('')
+            
+    context = {'login_form':form,
+               'age': 20}
+
+
+    return render(request, 'pages/login.html', context = context)
+
+
+#create a form
+def register(request):
+    form = CreateUserForm()
+
+    if request.method == "POST":
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('')
+    
+    context = {'form': form}
+    print(context)
+
+    return render(request, 'pages/register.html', context=context)
  
     
